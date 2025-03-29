56fedeb0068b419c5b541c0a14f97f84da502187
Weronika Górska <weronika.oliwia.gorska@gmail.com>
2025-03-29 15:24:27 +0100

    Add navigate page

---
+++ SwagLabsTestProject/Pom/Navigation.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwagLabsTestProject.Pom
+{
+    public class Navigation
+    {
+        private readonly IWebDriver _driver;
+
+        // Constructor to initialize the WebDriver
+        public Navigation(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        // Method to navigate to a specified URL
+        public void GoToUrl(string url)
+        {
+            _driver.Navigate().GoToUrl(url);
+        }
+    }
+}
